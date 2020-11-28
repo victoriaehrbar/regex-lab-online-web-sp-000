f71d1e9953893e60f643c7f426d7d36b0d9a7baf
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-28 00:34:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ text.match(/^[A-Z].*\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  phone.match(/^[(]?\d(3)[)]]?[\s]?\d(3)[\s-]?\d(4)$/) ? true : false
+  phone.match(/^[(]?\d(3)[)]?[\s]?\d(3)[\s-]?\d(4)$/) ? true : false
 end
